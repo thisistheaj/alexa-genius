5fa42b9324f7183606b3c5f4a19b1651f964108a
AJ Beckner <ajbeckner@gmail.com>
2016-10-22 15:13:17 -0400

    updated for cli use

--- publish.sh
+++ publish.sh
@@ -4,4 +4,4 @@ rm index.zip
 cd src
 zip -X -r ../index.zip *
 cd ..
-aws lambda update-function-code --function-name GetReady  --zip-file fileb://index.zip
+aws lambda update-function-code --function-name helloAlexa  --zip-file fileb://index.zip
