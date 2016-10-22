6d054a721c9cad6c7811c79b38a6efa8bc716419
AJ Beckner <ajbeckner@gmail.com>
2016-10-22 15:22:54 -0400

    moved dir structure

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
 /**
  * Created by me on 3/16/16.
  */
-
+var geniusWrapi = require('genius-wrapi');
+var client = new geniusWrapi("Bg0VtbkDCs6ryNbdEQyStts20hFUuTKHEaNhj0WRVJGnGrJJ4s4qoIFeyymoIt1S");
 var scrapper = require("./scrape.js");
 
 //Example
-scrapper.scrape("http://genius.com/Kendrick-lamar-swimming-pools-drank-lyrics");
+// scrapper.scrape("http://genius.com/Desiigner-panda-lyrics");
+
+client.search(
+    {
+        q:'Kendrick Lamar'
+    },
+    function(err, data) {
+        if (!err) {
+            console.log(data.response.hits[0].result.url);
+            scrapper.scrape(data.response.hits[0].result.url);
+
+        }
+    }
+);
 
 exports.handler = function( event, context ) {
 
     var response = {
         outputSpeech: {
             type: "PlainText",
-            text: "Hello World!"
+            text: "Hello Cli!"
         },
         shouldEndSession: true
     };
